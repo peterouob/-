701b4edd7f3379a225ab4982d89eb9f660d4b6e3
林庭楷 <hellolinpeter@gmail.com>
2024-10-23 18:55:46 +0800

    1

--- kuangbin带你飞/1/poj3279.cpp
+++ kuangbin带你飞/1/poj3279.cpp
@@ -1,96 +1,85 @@
-#include <iostream>
-#include <vector>
+#include <cstring>
 #include <stdio.h>
-
 using namespace std;
+const int INF = 0x3f3f3f;
+const int N = 15 + 5;
+int board[N][N], temp[N][N], vis[N][N], ans[N][N];
+int n, m;
+int min_step_ = INF;
 
-const int N = 15;
+int dict[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-vector<vector<int> > result;
-vector<vector<int> > flip;
-
-void convert(vector<vector<int> > &board, int n, int m, int x, int y) {
-    int dict[5][2] = {{0, 0}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    for (int i = 0; i < 5; i++) {
-        int dx = dict[i][0];
-        int dy = dict[i][1];
-        int nx = x + dx;
-        int ny = y + dy;
-        if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-            board[nx][ny] ^= 1;
-        }
+void reverse_(int u, int v) {
+  vis[u][v] = 1;
+  temp[u][v] ^= 1;
+  for (int i = 0; i < 4; i++) {
+    int x = u + dict[i][0];
+    int y = v + dict[i][1];
+    if (x >= 0 && x < n && y >= 0 && y < m) {
+      temp[x][y] ^= 1;
     }
+  }
 }
 
-int ans = 1e9;
-
-bool check_white(vector<vector<int> >&board,int n,int m){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(board[i][j] != 0) return false;
-        }
+bool check() {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (temp[i][j] == 1)
+        return false;
     }
-    return true;
+  }
+  return true;
 }
 
-void dfs(vector<vector<int> > &board, int n, int m, int pos, int flip_cnt) {
-    if (pos == n * m) {
-        if(check_white(board,n,m)){
-            if(flip_cnt < ans){
-                ans = flip_cnt;
-                result = flip;
-            }else if(flip_cnt == ans){
-                result = min(flip,result);
-            }
-        }
-        return;
+void slove(int x) {
+  memcpy(temp, board, sizeof(board));
+  memset(vis, 0, sizeof(vis));
+
+  int cnt = 0;
+  for (int i = 0; i < m; i++) {
+    // 狀態壓縮
+    if ((x >> i) & 1) {
+      cnt++;
+      reverse_(0, i);
     }
+  }
 
-    int x = pos / n;
-    int y = pos % n;
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (temp[i - 1][j] == 1) {
+        cnt++;
+        reverse_(i, j);
+      }
+    }
+  }
 
-    dfs(board, n, m, pos + 1, flip_cnt);
-
-    flip[x][y] = 1;
-    convert(board, n, m, x, y);
-    dfs(board, n, m, pos + 1, flip_cnt + 1);
-    convert(board, n, m, x, y);
-    flip[x][y] = 0;
+  if (check() && cnt < min_step_) {
+    min_step_ = cnt;
+    memcpy(ans, vis, sizeof(vis));
+  }
 }
-
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    int n=0, m=0;
-    while(scanf("%d%d",&n,&m) != EOF){
-        vector<vector<int> > board(n, vector<int>(m));
-
-        flip = vector<vector<int> >(n, vector<int>(m, 0));
-        result = vector<vector<int> >(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> board[i][j];
-            }
-        }
-
-        dfs(board, n, m, 0, 0);
-
-        if (ans == 1e9) {
-            cout << "IMPOSSIBLE" << endl;
-        } else {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    cout << result[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
+  scanf("%d%d", &n, &m);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      scanf("%d", &board[i][j]);
     }
+  }
 
+  // 枚舉
+  for (int i = 0; i < (1 << m); i++) {
+    slove(i);
+  }
 
-    return 0;
+  if (min_step_ == INF) {
+    printf("IMPOSSIBLE");
+  } else {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m - 1; j++) {
+        printf("%d ", ans[i][j]);
+      }
+      printf("%d\n", ans[i][m - 1]);
+    }
+  }
 }
